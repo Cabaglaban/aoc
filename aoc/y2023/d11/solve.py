cefd260808de74a37120e96722fb413e6f6ddc43
Jiří Dokládal <jiri.dokladal@firma.seznam.cz>
2023-12-11 12:38:58 +0100

    day11 refactored

--- aoc/y2023/d11/solve.py
+++ aoc/y2023/d11/solve.py
@@ -1,27 +1,27 @@
 from aoc.utils import *
 from itertools import combinations
+from itertools import chain
 
 
 def solve(input, n):
-    cols = [x for x in range(len(input[0])) if all(row[x] == "." for row in input)]
-    rows = [y for y, row in enumerate(input) if "#" not in row]
+    n = n - 1 if n > 1 else n
+    cols = [int(all(row[x] == "." for row in input)) for x in range(len(input[0]))]
+    rows = [int("#" not in row) for row in input]
     galaxies = [(x, y) for y, row in enumerate(input) for x, c in enumerate(row) if c == "#"]
 
-    def spaces(a, b):
-        xmin, xmax = min(a[0], b[0]), max(a[0], b[0])
-        ymin, ymax = min(a[1], b[1]), max(a[1], b[1])
+    def calc(a, b):
+        xmin, xmax, ymin, ymax = chain.from_iterable(sorted(i) for i in zip(a, b))
+        return (xmax - xmin) + (ymax - ymin) + n * (sum(cols[xmin:xmax]) + sum(rows[ymin:ymax]))
 
-        return sum(1 for c in cols if xmin < c < xmax) + sum(1 for r in rows if ymin < r < ymax)
-
-    return sum(abs(a[0] - b[0]) + abs(a[1] - b[1]) + n * spaces(a, b) for a, b in combinations(galaxies, 2))
+    return sum(calc(a, b) for a, b in combinations(galaxies, 2))
 
 
 runs = [
     (True, 1, "[part1] ex:"),
     (False, 1, "[part1] input:"),
-    (True, 10 - 1, "[part2] ex:"),
-    (True, 100 - 1, "[part2] ex:"),
-    (False, 1_000_000 - 1, "[part2] input:"),
+    (True, 10, "[part2] ex:"),
+    (True, 100, "[part2] ex:"),
+    (False, 1_000_000, "[part2] input:"),
 ]
 
 for example, spaces_mul, run in runs:
